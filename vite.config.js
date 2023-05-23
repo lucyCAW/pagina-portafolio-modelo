04ed647f68b4585daa2730a77c524a1b7d6748ab
lucia <luciapachano2014@gmail.com>
2023-05-22 23:26:30 -0400

    fix url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-web/",
+  base: "/pagina-portafolio-modelo/",
 })
